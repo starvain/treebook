4c9ea9801999ede190bf25fe9fd10759fbdf1a72
Seppo Tarvainen <tarvainen.seppo@gmail.com>
2014-03-01 00:06:16 +0200

    Install devise, add a user, and install devise views.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Joku herokujuttu? Tuli devise gemin mukana?
+    config.assets.initialize_on_precompile = false
   end
 end
